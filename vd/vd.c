5fb48f2fe72762a78e2869d3222ccf674acf9212
Yu Jing <yujing5b5d@gmail.com>
2013-03-25 17:09:53 +0800

    add

--- vd/vd.c
+++ vd/vd.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include "vd.h"
+
+const char tokenStr[][10] = {"RefA","SinRef"}; 
+
+int parseFile(const char * fileName)
+{
+	FILE *fp = fopen(fileName,"r");
+	fclose(fp);
+}
+
+const char *test_file="data/Database.txt";
 
 int main(int argc,char *argv[])
 {
