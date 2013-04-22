6d51bf890b740f5992330745f6aa918962707185
Yu Jing <yujing5b5d@gmail.com>
2013-04-22 17:44:24 +0800

    auto:20130422174424

--- EndOfReferences/src/debuginfo.c
+++ EndOfReferences/src/debuginfo.c
@@ -7,7 +7,7 @@ int printfContext(int offset)
 	int x;
 	
 	printf("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
-	for(x=(offset<100)?0:(offset-100);x<offset;x++) putchar(*(getPcontent()+x));
+	for(x=(offset=<100)?0:(offset-100);x<offset;x++) putchar(*(getPcontent()+x));
 	printf("\n================================================================\n");
 	for(x=offset;x<offset+100 && x < getPclen();x++) putchar(*(getPcontent()+x));
 	printf("\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n");
