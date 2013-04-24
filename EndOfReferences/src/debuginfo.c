784f3c43e39813b49707ea50c0ac93f24e4701e9
Yu Jing <yujing5b5d@gmail.com>
2013-04-24 15:01:10 +0800

    auto:20130424150110

--- EndOfReferences/src/debuginfo.c
+++ EndOfReferences/src/debuginfo.c
@@ -19,7 +19,7 @@ int printfContext(int offset)
 int printfContextS(int offset,const char *str)
 {
 	int status;
-	printf("\n$TITIL(%s){\n",str);
+	printf("\n$TITIL(%s)@[%d]{\n",str,offset);
 	status = printfContext(offset);
 	printf("}\n");
 	return status;
