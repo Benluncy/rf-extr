460adf667faa7df785c515ae843ff370daac451c
Yu Jing <yujing5b5d@gmail.com>
2013-04-28 13:35:32 +0800

    another check

--- checkdiff/checkdiff.c
+++ checkdiff/checkdiff.c
@@ -4,11 +4,41 @@
 
 #define LINELEN 2048
 
+char *doEscape(char *str)
+{
+	int len = strlen(str);
+	int j=0;
+	static char mstr[1024];
+	for(int i=0;i<len;i++)
+	{
+		if(str[i]=='\"')
+		{
+			mstr[j] = '\\';
+			j++;
+		}
+		mstr[j] = str[i];
+		j++;
+	}
+	mstr[j] = '\0';
+	j--;
+	while(mstr[j]==' ' || mstr[j]=='\\' || mstr[j]=='\n' || mstr[j]=='\r')
+	{
+		mstr[j] = '\0';
+		j--;
+	}
+	return mstr;
+	
+}
+
 int main(int argc,char *argv[])
 {
 	FILE *fpTestfile = fopen("test.txt","r");
 	FILE *fpPredictfile = fopen("svm_predictions","r");
 	FILE *fpResultfile = fopen("diff.log","w");
+	const char *btu="/home/yu/Workspace/cc/features/GetContext/main ";
+	//const char *path="/home/yu/Workspace/cc/features/GetContext/";
+	
+	char command[1024];
 	if(argc > 1)
 	{
 		if(fpTestfile != NULL) fclose(fpTestfile);
@@ -46,10 +76,35 @@ int main(int argc,char *argv[])
 			}
 		}while(strTestFileContent[0] == '#');
 		if((strTestFileContent[0]=='-') ^ (strPredictFileContent[0]=='-'))
+		//if(1)
 		{
 			diff++;
-			fprintf(fpResultfile,"%s",paperName);
+			fprintf(fpResultfile,"%s",paperName+2);
 			fprintf(fpResultfile,"%d:%s%s\n",xx,strPredictFileContent,strTestFileContent);
+			
+			int slen = strlen(strTestFileContent);
+			int dOffset = 0;
+			for(int z=0;z<slen;z++)
+			{
+				if(strTestFileContent[z]=='#')
+				{
+					dOffset = atoi(strTestFileContent+z+1);
+					break;
+				}
+				
+			}
+			sprintf(command,"%s \"%s\" %d",btu,doEscape(paperName+2),dOffset);
+			if(dOffset!=0)
+			{
+				//printf("run: {%s}\n",command);
+				//printf("content: {%s}\n",strTestFileContent);
+				//break;
+				printf("%s",paperName+2);
+				printf("%d:%s%s\n",xx,strPredictFileContent,strTestFileContent);
+				system(command);
+				break;
+			}
+				
 		}
 	}
 	printf("diff / total : %d / %d (%f)\n",diff,total,(double)(total - diff)/total);
