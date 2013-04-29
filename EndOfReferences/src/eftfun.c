c7eb1a832c1b5e6b640db6610961496753809a85
Yu Jing <yujing5b5d@gmail.com>
2013-04-29 12:43:28 +0800

    auto:20130429124328

--- EndOfReferences/src/eftfun.c
+++ EndOfReferences/src/eftfun.c
@@ -995,7 +995,7 @@ int genNextDataForEndfeature(FILE *fp,endFeatureData fd,int start)
 
 int prepareDensityData(void)
 {
-	return 1;
+	//return 1;
 	int beforeData;
 	int totalData;
 	for(int i=0;i<mfdc.top;i++)
