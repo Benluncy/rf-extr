a804b1362ef89bfa2c9cc4771f55fff2804ff084
Yu Jing <yujing5b5d@gmail.com>
2013-04-29 15:43:42 +0800

    auto:20130429154342

--- EndOfReferences/src/eftctl.c
+++ EndOfReferences/src/eftctl.c
@@ -186,7 +186,7 @@ int genEndSampleCtl(const char* fileName,int isDir)
 				// 0 , 1
 				fprintf(fp,"%d:%d ",start++,_mfdc->data[i].t[j]);
 				break;
-			/*
+			
 			case 9:
 			case 12:
 			case 15:
@@ -195,6 +195,7 @@ int genEndSampleCtl(const char* fileName,int isDir)
 				rankWrite(fp,start,_mfdc->data[i].t[j],3);
 				start += 3;
 				break;
+			/*
 			case 21:
 			case 24:
 			case 27:
