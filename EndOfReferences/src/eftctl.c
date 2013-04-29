f89c1b7cd657825c085b2e07409e878910afeb83
Yu Jing <yujing5b5d@gmail.com>
2013-04-29 18:53:20 +0800

    auto:20130429185320

--- EndOfReferences/src/eftctl.c
+++ EndOfReferences/src/eftctl.c
@@ -237,10 +237,10 @@ int genEndSampleCtl(const char* fileName,int isDir)
 		}
 		
 		rankWrite(fp,start,_mfdc->data[i].t[7]+start,_mfdc->data[i].t[8]
-			+start,_mfdc->data[i].t[10]+start,_mfdc->data[i].t[11]
-			+start,_mfdc->data[i].t[13]+start,_mfdc->data[i].t[14]
-			+start,_mfdc->data[i].t[3]+start,_mfdc->data[i].t[4]
-			+start,_mfdc->data[i].t[5],8);
+			+_mfdc->data[i].t[10]+start,_mfdc->data[i].t[11]
+			+_mfdc->data[i].t[13]+start,_mfdc->data[i].t[14]
+			+_mfdc->data[i].t[3]+start,_mfdc->data[i].t[4]
+			+_mfdc->data[i].t[5],8);
 		start += 8;
 			
 		start = genNextDataForEndfeature(fp,_mfdc->data[i],start);
