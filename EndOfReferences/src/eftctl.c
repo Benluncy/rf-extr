24844c40742004fc4eea7bf3815708629d41295b
Yu Jing <yujing5b5d@gmail.com>
2013-04-29 18:52:44 +0800

    auto:20130429185244

--- EndOfReferences/src/eftctl.c
+++ EndOfReferences/src/eftctl.c
@@ -199,7 +199,7 @@ int genEndSampleCtl(const char* fileName,int isDir)
 				break;
 			// */
 			
-			//*
+			/*
 			// f 4
 			case 7:
 			case 8:
@@ -213,6 +213,7 @@ int genEndSampleCtl(const char* fileName,int isDir)
 			case 15:
 				// 9 end year before ack or table
 				// 12 end page before ack or table
+				// end year before article
 				// 0 , 1 , 2,
 				rankWrite(fp,start,_mfdc->data[i].t[j],3);
 				start += 3;
@@ -234,6 +235,13 @@ int genEndSampleCtl(const char* fileName,int isDir)
 		
 			
 		}
+		
+		rankWrite(fp,start,_mfdc->data[i].t[7]+start,_mfdc->data[i].t[8]
+			+start,_mfdc->data[i].t[10]+start,_mfdc->data[i].t[11]
+			+start,_mfdc->data[i].t[13]+start,_mfdc->data[i].t[14]
+			+start,_mfdc->data[i].t[3]+start,_mfdc->data[i].t[4]
+			+start,_mfdc->data[i].t[5],8);
+		start += 8;
 			
 		start = genNextDataForEndfeature(fp,_mfdc->data[i],start);
 		fprintf(fp," #%d",_mfdc->data[i].offset);
