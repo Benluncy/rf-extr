af1f00cfb289499af34fad20cc2d97628b7c6a2c
Yu Jing <yujing5b5d@gmail.com>
2013-05-01 20:11:45 +0800

    auto:20130501201145

--- EndOfReferences/src/hftctl.c
+++ EndOfReferences/src/hftctl.c
@@ -241,6 +241,16 @@ int rankWrite(FILE *fp,int start,int rank,int len)
 	return 1;
 }
 
+int rankWriteNoMore(FILE *fp,int start,int rank,int len)
+{
+	
+	for(int i=0;i<len;i++)
+	{
+		fprintf(fp,"%d:%d ",start+i,rank == i+1 ? 1 : 0);
+	}
+	return 1;
+}
+
 inline int offsetStat(int offset,int *totalData,int *beforeData,
 					OffsetCallback offsetFunction)
 {
