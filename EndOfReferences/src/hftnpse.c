ecabf6d627154e989bc1751a51d93e4478b06cb5
Yu Jing <yujing5b5d@gmail.com>
2013-05-05 18:59:53 +0800

    auto:20130505185953

--- EndOfReferences/src/hftnpse.c
+++ EndOfReferences/src/hftnpse.c
@@ -559,18 +559,21 @@ int hasSpecialKeyWords(int offset,int limit) // in references
 			
 			//23(3) // volume
 			int z = i;
+			int tmp = z;
 			while(fitPattern('n',content[z]) && z < offend -2)
 			{
 				z++;
+				if(z>=offend || z-tmp > 2) break;
 			}
 			if(z>=offend-2) break;
 			if(content[z] == '(')
 			{
 				z++;
+				tmp = z;
 				while(fitPattern('n',content[z]))
 				{
 					z++;
-					if(z>=offend) break;
+					if(z>=offend || z-tmp > 2) break;
 				}
 				if(z>=offend) break;
 				if(content[z] == ')') return i+z+1;
