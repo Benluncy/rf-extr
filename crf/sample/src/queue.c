02e49baa04e782badf790378f914005bc52179f1
Yu Jing <yujing5b5d@gmail.com>
2013-05-16 18:04:42 +0800

    auto:20130516180442

--- crf/sample/src/queue.c
+++ crf/sample/src/queue.c
@@ -1,31 +1,22 @@
 #include "queue.h"
 #include <stdio.h> // NULL
 
-void clearQueue(pCNSQ Q)
-{ 
-    Q->front=Q->rear=0;
-}
+inline void clearQueue(pCNSQ Q){Q->front=Q->rear=0;}
 
 // test if the queue is empty
-int isEmptyQueue(pCNSQ Q)
-{
-    return Q->front==Q->rear ;
-}
+inline int isEmptyQueue(pCNSQ Q){return Q->front==Q->rear ;}
 
-int isFullQueue(pCNSQ Q)
-{
-    return (Q->rear+1)%CNSQSIZE==Q->front;
-}
+inline int isFullQueue(pCNSQ Q){ return (Q->rear+1)%CNSQSIZE==Q->front;}
 
 int enQueue(pCNSQ Q,CrfNodeSnapshot e)
 {
-    if((Q->rear+1)%CNSQSIZE==Q->front){ // full
-    	return 0;
-    }else{ 
-        Q->rear=(Q->rear+1)%CNSQSIZE;
-        Q->data[Q->rear]=e;
-        return 1;
-    }
+	if((Q->rear+1)%CNSQSIZE==Q->front){ // full
+		return 0;
+	}else{
+		Q->rear=(Q->rear+1)%CNSQSIZE;
+		Q->data[Q->rear]=e;
+		return 1;
+	}
 }
 
 int enQueueWithDrop(pCNSQ Q,CrfNodeSnapshot e)
@@ -37,38 +28,40 @@ int enQueueWithDrop(pCNSQ Q,CrfNodeSnapshot e)
 
 pCrfNodeSnapshot deQueue(pCNSQ Q)
 {    
-    if(isEmptyQueue(Q)){ // empty
-        return NULL;
-    }else {
-        Q->front=(Q->front+1)%CNSQSIZE;
-        return &(Q->data[Q->front]);
-    }
+	if(isEmptyQueue(Q)){ // empty
+		return NULL;
+	}else {
+		Q->front=(Q->front+1)%CNSQSIZE;
+		return &(Q->data[Q->front]);
+	}
 }
 
 pCrfNodeSnapshot nextNElem(pCNSQ Q,int N) // next as queue
 {
-    if((Q->front+N)%CNSQSIZE >=Q->rear){ // || N >= CNSQSIZE || N < 1){ // no such
-        return NULL;
-    }else {
-        return &(Q->data[(Q->front+N)%CNSQSIZE]);
-    }	
+	//if((Q->front+N)%CNSQSIZE ==Q->rear ){ // || N >= CNSQSIZE || N < 1){ // no such
+	if(N > sizeQueue(Q)){
+		return NULL;
+	}else {
+		return &(Q->data[(Q->front+N)%CNSQSIZE]);
+	}
 }
 
 pCrfNodeSnapshot pastNElem(pCNSQ Q,int N) // as stack
 {
-    if((Q->front+N)%CNSQSIZE >=Q->rear || N >= CNSQSIZE || N < 1){ // no such
-        return NULL;
-    }else {
-        return &(Q->data[(Q->rear+CNSQSIZE-N+1)%CNSQSIZE]);
-    }
+	//if((Q->front+N)%CNSQSIZE >=Q->rear || N >= CNSQSIZE || N < 1){ // no such
+	if(N > sizeQueue(Q)){
+		return NULL;
+	}else {
+		return &(Q->data[(Q->rear+CNSQSIZE-N+1)%CNSQSIZE]);
+	}
 }
 
 
 
 int sizeQueue(pCNSQ Q)
 {
-    int i=(Q->rear-Q->front+CNSQSIZE)%CNSQSIZE;
-    return i;
+	int i=(Q->rear-Q->front+CNSQSIZE)%CNSQSIZE;
+	return i;
 }
 
 
