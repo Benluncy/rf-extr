d4a2cda3b9e3a8df859ca3826a109764408d60f9
Yu Jing <yujing5b5d@gmail.com>
2013-05-28 06:22:13 +0800

    auto:20130528062213

--- crf/sample/src/crftctl.c
+++ crf/sample/src/crftctl.c
@@ -177,7 +177,7 @@ int genCRFSampleCtl(const char* fileName,int isDir)
 				if(tCNS->stopflag  == 1) stopEffect = (stopEffect == 2) ? 2 : 1;
 				if(tCNS->stopflag  == 2) stopEffect = 2;
 			}
-			if(tCNS->puredigit == 1)
+			if(tCNS->puredigit == 2 )
 			{
 				edNoStop = 0;
 			}
@@ -325,7 +325,7 @@ int genCRFSampleCtl(const char* fileName,int isDir)
 			{
 				domainNoStop = 0;
 			}
-			if(tCNS->puredigit == 1)
+			if(tCNS->puredigit == 2 )
 			{
 				edNoStop = 0;
 			}
