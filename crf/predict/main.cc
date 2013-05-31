7c811bb18431c62057a13f774b9a22e44fce8589
Yu Jing <yujing5b5d@gmail.com>
2013-05-31 16:15:51 +0800

    auto:20130531161551

--- crf/predict/main.cc
+++ crf/predict/main.cc
@@ -21,15 +21,28 @@ void printCitation(pCitationNode p)
 	std::cout << "------------------" << std::endl;
 	for(i=0;i<10;i++)
 	{
-		if(p->author[i][0] != 0) std::cout << "author("<<(i+1)
-				<<"): " << p->author[i] << std::endl;
+		if(p->author[i][0] != 0) std::cout << "author["<<(i+1)
+				<<"]: \t" << p->author[i] << std::endl;
 		else break;
 	}
+	
+	if(p->title[0] != 0) std::cout << "title:\t" << p->title << std::endl;
+	if(p->journal[0] != 0) std::cout << "journal:\t" << p->journal << std::endl;
+	if(p->booktitle[0] != 0) std::cout << "booktitle:\t" << p->booktitle << std::endl;
+	if(p->publisher[0] != 0) std::cout << "publisher:\t" << p->publisher << std::endl;
+	if(p->institution[0] != 0) std::cout << "institution:\t" << p->institution << std::endl;
+	if(p->volume[0] != 0) std::cout << "volume:\t" << p->volume << std::endl;
+	if(p->location[0] != 0) std::cout << "location:\t" << p->location << std::endl;
+	if(p->pages[0] != 0) std::cout << "pages:\t" << p->pages << std::endl;
+	if(p->date[0] != 0) std::cout << "date:\t" << p->date << std::endl;
+	if(p->url[0] != 0) std::cout << "url:\t" << p->url << std::endl;
+	if(p->isbn[0] != 0) std::cout << "isbn:\t" << p->isbn << std::endl;
+	if(p->tech[0] != 0) std::cout << "tech:\t" << p->tech << std::endl;
 	
 	for(i=0;i<10;i++)
 	{
 		if(p->editor[i][0] != 0) std::cout << "editor("<<(i+1)
-				<<"): " << p->editor[i] << std::endl;
+				<<"): \t" << p->editor[i] << std::endl;
 		else break;
 	}
 	
